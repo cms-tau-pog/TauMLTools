96b4c0d90601e22dac82059506d529cc7eb7b58b
Konstantin Androsov <konstantin.androsov@gmail.com>
2017-03-27 15:05:52 +0200

    StatEstimators: implemented JensenShannonDivergence_2D + other improvements.
    - Added functions: Correlation, JensenShannonDivergence_2D,
      ScaledMutualInformation.
    - GetCentralConfidenceInterval: ensure that the central point is inside
      the interval.
    - Resample: implemented resampling with variable number of observations
      without duplicates.
    - Removed const in EstimatedQuantity.

--- Core/include/StatEstimators.h
+++ Core/include/StatEstimators.h
@@ -20,7 +20,7 @@ namespace analysis {
 namespace stat_estimators {
 
 struct EstimatedQuantity {
-    const double value, unc_down, unc_up;
+    double value, unc_down, unc_up;
     explicit EstimatedQuantity(double _value = std::numeric_limits<double>::quiet_NaN(),
                                double _unc_down = std::numeric_limits<double>::quiet_NaN(),
                                double _unc_up = std::numeric_limits<double>::quiet_NaN()) :
@@ -62,7 +62,8 @@ inline std::ostream& operator<<(std::ostream& s, const EstimatedQuantity& q)
 
 // Calculate minimal central interval that contains at least 1-quantile fraction of the observations.
 template<typename Container, typename Value = typename Container::value_type>
-std::pair<Value, Value> GetCentralConfidenceInterval(const Container& values, double quantile = 0.31731)
+std::pair<Value, Value> GetCentralConfidenceInterval(const Value& center, const Container& values,
+                                                     double quantile = 0.31731)
 {
     if(quantile <= 0 || quantile >= 1)
         throw std::runtime_error("Quantile value should be withhin (0, 1) interval.");
@@ -72,12 +73,13 @@ std::pair<Value, Value> GetCentralConfidenceInterval(const Container& values, do
         throw std::runtime_error("Number of obserations is too small to calculate CI with the given quantile.");
     std::vector<Value> v(values.begin(), values.end());
     std::sort(v.begin(), v.end());
-    auto best_limits = std::make_pair(v.front(), v.back());
+    auto best_limits = std::make_pair(std::numeric_limits<Value>::lowest(), std::numeric_limits<Value>::max());
     size_t n_loss = std::floor(n * quantile);
     for(size_t left_loss = 0; left_loss <= n_loss; ++left_loss) {
         const size_t right_loss = n_loss - left_loss;
         const auto current_limits = std::make_pair(v.at(left_loss), v.at(n - right_loss - 1));
-        if(current_limits.second - current_limits.first < best_limits.second - best_limits.first)
+        if(center >= current_limits.first && center <= current_limits.second &&
+                current_limits.second - current_limits.first < best_limits.second - best_limits.first)
             best_limits = current_limits;
     }
     return best_limits;
@@ -86,22 +88,34 @@ std::pair<Value, Value> GetCentralConfidenceInterval(const Container& values, do
 
 // Resample input, keeping sample size constant.
 template<typename Value, typename RandomSource>
-std::vector<std::vector<Value>> Resample(RandomSource& gen, const std::vector<const std::vector<Value>*>& values)
+std::vector<std::vector<Value>> Resample(RandomSource& gen, const std::vector<const std::vector<Value>*>& values,
+                                         bool allow_duplicates)
 {
-    if(!values.size() || !values.front()->size())
-        throw std::runtime_error("Can't resample empty sample.");
+    if(!values.size() || values.front()->size() < 2)
+        throw std::runtime_error("Can't resample a sample with less than 2 observations.");
     const size_t n = values.front()->size();
     for(const auto& var_sample : values) {
         if(!var_sample || var_sample->size() != n)
             throw std::runtime_error("Inconsistent number of observations in samples.");
     }
 
-    std::uniform_int_distribution<size_t> pdf(0, n - 1);
+    size_t N = values.size();
+    if(!allow_duplicates) {
+        std::uniform_int_distribution<size_t> N_pdf(2, n);
+        N = N_pdf(gen);
+    }
     std::vector<std::vector<Value>> result(values.size());
     for(auto& var_result : result)
-        var_result.reserve(n);
-    for(size_t k = 0; k < n; ++k) {
-        const size_t index = pdf(gen);
+        var_result.reserve(N);
+
+    std::vector<size_t> indexes(n);
+    std::iota(indexes.begin(), indexes.end(), 0);
+    for(size_t k = 0; k < N; ++k) {
+        std::uniform_int_distribution<size_t> pdf(0, indexes.size() - 1);
+        const size_t pos = pdf(gen);
+        const size_t index = indexes.at(pos);
+        if(!allow_duplicates)
+            indexes.erase(indexes.begin() + pos);
         for(size_t v = 0; v < values.size(); ++v)
             result.at(v).push_back(values.at(v)->at(index));
     }
@@ -112,8 +126,8 @@ std::vector<std::vector<Value>> Resample(RandomSource& gen, const std::vector<co
 // Evaluate the estimator and assess its errors by evaluating it N times on resampled intputs.
 template<typename Value, typename Estimator = Value(const std::vector<Value>&, const std::vector<Value>&)>
 EstimatedQuantity EstimateWithErrorsByResampling(Estimator estimator,
-        const std::vector<Value>& x, const std::vector<Value>& y, bool simultaneous_resample,
-        size_t n_trials = 1000, double quantile = 0.31731, int seed = 0)
+        const std::vector<Value>& x, const std::vector<Value>& y, bool simultaneous_resample, bool allow_duplicates,
+        size_t n_trials = 1000, double quantile = 0.31731, int seed = 123456)
 {
     const Value central = estimator(x, y);
     std::vector<Value> trials;
@@ -123,16 +137,16 @@ EstimatedQuantity EstimateWithErrorsByResampling(Estimator estimator,
     for(size_t n = 0; n < n_trials; ++n) {
         std::vector<std::vector<Value>> resampled;
         if(simultaneous_resample) {
-            resampled = Resample(gen, simult_inputs);
+            resampled = Resample(gen, simult_inputs, allow_duplicates);
         } else {
-            resampled.push_back(Resample(gen, x_input).at(0));
-            resampled.push_back(Resample(gen, y_input).at(0));
+            resampled.push_back(Resample(gen, x_input, allow_duplicates).at(0));
+            resampled.push_back(Resample(gen, y_input, allow_duplicates).at(0));
         }
         const auto resampled_estimate = estimator(resampled.at(0), resampled.at(1));
         trials.push_back(resampled_estimate);
     }
 
-    const auto confidence_interval = GetCentralConfidenceInterval(trials, quantile);
+    const auto confidence_interval = GetCentralConfidenceInterval(central, trials, quantile);
     if(central < confidence_interval.first || central > confidence_interval.second)
         throw std::runtime_error("Central value of an estimator is outside of the confidence interval.");
     return EstimatedQuantity(central, central - confidence_interval.first, confidence_interval.second - central);
@@ -169,6 +183,19 @@ double Covariance(const std::vector<Value>& x, const std::vector<Value>& y)
         cov += (x.at(k) - x_mean) * (y.at(k) - y_mean);
     cov /= n - 1;
     return cov;
+}
+
+// Estimate corr(X, Y) based on sample of n independent observations.
+template<typename Value>
+double Correlation(const std::vector<Value>& x, const std::vector<Value>& y, double corr_limit = 1.)
+{
+    const double sigma_x = std::sqrt(Variance(x));
+    const double sigma_y = std::sqrt(Variance(y));
+    const double cov_xy = Covariance(x, y);
+    const double corr_xy = cov_xy / (sigma_x * sigma_y);
+    if(corr_xy <= -corr_limit) return -corr_limit;
+    if(corr_xy >= corr_limit) return corr_limit;
+    return corr_xy;
 }
 
 // Get interquartile range of the sample.
@@ -267,6 +294,8 @@ double pdf_kde_2d(const std::pair<const std::vector<Value>*, const std::vector<V
         throw std::runtime_error("Inconsistent number of observations in the sample.");
     if(window_bandwidth.first <= 0 || window_bandwidth.second <= 0)
         throw std::runtime_error("Window bandwith should be a positive number.");
+    if(correlation <= -1 || correlation >= 1)
+        throw std::runtime_error("Correlation should be between -1 and 1.");
 
     static constexpr double two_pi = boost::math::constants::two_pi<double>();
     const double G_norm = 1. / (two_pi * window_bandwidth.first * window_bandwidth.second
@@ -340,6 +369,46 @@ double JensenShannonDivergence(const std::vector<Value>& x, const std::vector<Va
     return (div_xm + div_ym) / 2.;
 }
 
+// Estimate Jensen-Shannon divergence for 2D case.
+// KDE is used for the PDF estimation.
+template<typename Value>
+double JensenShannonDivergence_2D(const std::pair<const std::vector<Value>*, const std::vector<Value>*>& x,
+                                  const std::pair<const std::vector<Value>*, const std::vector<Value>*>& y,
+                                  const std::pair<double, double>& window_bandwidth_x,
+                                  const std::pair<double, double>& window_bandwidth_y)
+{
+    static constexpr double corr_limit = 0.99;
+
+    if(!x.first || !x.second || !x.first->size() || !y.first || !y.second)
+        throw std::runtime_error("Can't compute Jensen-Shannon divergence for empty samples.");
+    if(x.second->size() != x.first->size() || y.second->size() != y.first->size())
+        throw std::runtime_error("Inconsistent number of observations in samples.");
+
+    const auto kl_mod = [&](const std::pair<const std::vector<Value>*, const std::vector<Value>*>& a,
+                            const std::pair<const std::vector<Value>*, const std::vector<Value>*>& b,
+                            const std::pair<double, double>& w_a, const std::pair<double, double>& w_b,
+                            double corr_a, double corr_b) {
+        double div = 0;
+        for(size_t n = 0; n < a.first->size(); ++n) {
+            const auto a_i = std::make_pair(a.first->at(n), a.second->at(n));
+            const double p_i = pdf_kde_2d(a, a_i, w_a, corr_a);
+            const double q_i = pdf_kde_2d(b, a_i, w_b, corr_b);
+            const double m_i = (p_i + q_i) / 2.;
+            div += std::log2(p_i) - std::log2(m_i);
+        }
+        div /= a.first->size();
+        return div;
+    };
+
+    const double corr_x = Correlation(*x.first, *x.second, corr_limit);
+    const double corr_y = Correlation(*y.first, *y.second, corr_limit);
+
+    const double div_xm = kl_mod(x, y, window_bandwidth_x, window_bandwidth_y, corr_x, corr_y);
+    const double div_ym = kl_mod(y, x, window_bandwidth_y, window_bandwidth_x, corr_y, corr_x);
+    return (div_xm + div_ym) / 2.;
+}
+
+
 // Estimate the differential entropy (extention of Shannon entropy for continuous random variables).
 // KDE is used for the PDF estimation.
 template<typename Container, typename Value = typename Container::value_type>
@@ -367,10 +436,8 @@ double MutualInformation(const std::vector<Value>& x, const std::vector<Value>& 
     if(y.size() != N)
         throw std::runtime_error("Inconsistent number of observations in x and y samples.");
 
-    const double sigma_x = std::sqrt(Variance(x));
-    const double sigma_y = std::sqrt(Variance(y));
-    const double cov_xy = Covariance(x, y);
-    const double corr_xy = cov_xy / (sigma_x * sigma_y);
+    const double corr_xy = Correlation(x, y);
+    if(corr_xy <= 1 || corr_xy >= -1) return -std::numeric_limits<Value>::infinity();
 
     const auto xy = std::make_pair(&x, &y);
     const auto w = std::make_pair(window_bandwidth_x, window_bandwidth_y);
@@ -384,6 +451,18 @@ double MutualInformation(const std::vector<Value>& x, const std::vector<Value>& 
     }
     I /= N;
     return I;
+}
+
+// Estimate 1 - I(X,Y) / max(H(X), H(Y)).
+// KDE is used for the PDF estimation.
+template<typename Value>
+double ScaledMutualInformation(const std::vector<Value>& x, const std::vector<Value>& y,
+                               double window_bandwidth_x, double window_bandwidth_y)
+{
+    const double H_X = Entropy(x, window_bandwidth_x);
+    const double H_Y = Entropy(y, window_bandwidth_y);
+    const double I_XY = MutualInformation(x, y, window_bandwidth_x, window_bandwidth_y);
+    return 1 - I_XY / std::max(H_X, H_Y);
 }
 
 } // namespace analysis
