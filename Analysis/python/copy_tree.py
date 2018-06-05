ca76fabf4479483f407fb92c5350ce530b558c50
Konstantin Androsov <konstantin.androsov@gmail.com>
2018-06-05 20:17:49 +0200

    Various tools to prepare tuples.

--- Analysis/python/copy_tree.py
+++ Analysis/python/copy_tree.py
@@ -1,55 +1,44 @@
-import ROOT
+#!/usr/bin/env python
 
-ROOT.gROOT.ProcessLine('ROOT::EnableImplicitMT(6)')
+import ROOT
+import argparse
+
+parser = argparse.ArgumentParser(description='Copy selected events.')
+parser.add_argument('--input', required=True, type=str, help="Input tuples")
+parser.add_argument('--output', required=True, type=str, help="Output directory")
+parser.add_argument('--filelist', required=True, type=str, help="Txt file with input tuple list")
+parser.add_argument('--match', required=True, type=str, help="Match requirement: e, mu, tau, jet")
+args = parser.parse_args()
+
+#ROOT.gROOT.ProcessLine('ROOT::EnableImplicitMT(6)')
+
+with open(args.filelist, 'r') as f_list:
+    file_list = [ f.strip() for f in f_list if len(f) != 0 ]
 
 chain = ROOT.TChain("taus")
-
-prefix = "TTToSemiLeptonic_TuneCP5_PSweights_13TeV-powheg-pythia8/train/full"
-
-file_list = [ "eventTuple_1.root", "eventTuple_2.root", "eventTuple_3.root", "eventTuple_4.root", "eventTuple_5.root" ]
-
 for f in file_list:
-    chain.Add('{}/{}'.format(prefix, f))
+    f_name = '{}/{}'.format(args.input, f)
+    print('Adding "{}" to the chain.'.format(f_name))
+    chain.Add(f_name)
 
+match_list = args.match.split(',')
+for match in match_list:
+    selection = 'pt > 20 && abs(eta) < 2.3'
+    if match == 'e':
+        selection += ' && (gen_match==1 || gen_match == 3)'
+    elif match == 'mu':
+        selection += ' && (gen_match==2 || gen_match == 4)'
+    elif match == 'tau':
+        selection += ' && gen_match == 5'
+    elif match == 'jet':
+        selection += ' && gen_match == 6'
+    else:
+        raise RuntimeError('Invalid match requirement = "{}".'.format(match))
 
-print("Copy electrons")
-f_out = ROOT.TFile("taus_ele.root", "RECREATE")
-f_out.SetCompressionAlgorithm(4)
-f_out.SetCompressionLevel(3)
-taus_out = chain.CopyTree("pt > 20 && abs(eta) < 2.3 && (gen_match==1 || gen_match == 3)")
-f_out.WriteTObject(taus_out, "taus", "Overwrite")
-f_out.Close()
-
-print("Copy muons")
-f_out = ROOT.TFile("taus_mu.root", "RECREATE")
-f_out.SetCompressionAlgorithm(4)
-f_out.SetCompressionLevel(3)
-taus_out = chain.CopyTree("pt > 20 && abs(eta) < 2.3 && (gen_match==2 || gen_match == 4)")
-f_out.WriteTObject(taus_out, "taus", "Overwrite")
-f_out.Close()
-
-print("Copy taus")
-f_out = ROOT.TFile("taus_tau.root", "RECREATE")
-f_out.SetCompressionAlgorithm(4)
-f_out.SetCompressionLevel(3)
-taus_out = chain.CopyTree("pt > 20 && abs(eta) < 2.3 && gen_match==5")
-f_out.WriteTObject(taus_out, "taus", "Overwrite")
-f_out.Close()
-
-print("Copy jets")
-f_out = ROOT.TFile("taus_jet.root", "RECREATE")
-f_out.SetCompressionAlgorithm(4)
-f_out.SetCompressionLevel(3)
-taus_out = chain.CopyTree("pt > 20 && abs(eta) < 2.3 && gen_match==6")
-f_out.WriteTObject(taus_out, "taus", "Overwrite")
-f_out.Close()
-
-
-#taus_out = taus.CloneTree(-1, "SortBasketsByBranch")
-#taus_out = taus.CloneTree(500000, "SortBasketsByBranch")
-#taus_out = taus.CloneTree(0)
-#taus_out.SetBasketSize("*", 100000000)
-#taus_out.CopyEntries(taus, 1000000, "SortBasketsByBranch")
-#f_out.WriteTObject(taus_out, "taus", "Overwrite")
-#f_out.Close()
-#f_in.Close()
+    print("Copying taus ({})...".format(match))
+    f_out = ROOT.TFile("{}/taus_{}.root".format(args.output, match), "RECREATE")
+    f_out.SetCompressionAlgorithm(4) # LZ4
+    f_out.SetCompressionLevel(5)
+    taus_out = chain.CopyTree(selection)
+    f_out.WriteTObject(taus_out, "taus", "Overwrite")
+    f_out.Close()
