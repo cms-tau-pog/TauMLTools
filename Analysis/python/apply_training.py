8caa73f854f638b8608caf5daa4281bccf2e04fb
Konstantin Androsov <konstantin.androsov@gmail.com>
2018-06-14 11:13:26 +0200

    Improved DeepTau_v2.
    Former-commit-id: 456f6b30d8ff1890856f66a701ad44f72d6f7dd3

--- Analysis/python/apply_training.py
+++ Analysis/python/apply_training.py
@@ -3,11 +3,12 @@
 import argparse
 parser = argparse.ArgumentParser(description='Apply training and store results.')
 parser.add_argument('--input', required=True, type=str, help="Input directory")
-parser.add_argument('--filelist', required=True, type=str, help="Txt file with input tuple list")
+parser.add_argument('--filelist', required=False, type=str, default=None, help="Txt file with input tuple list")
 parser.add_argument('--output', required=True, type=str, help="Output file")
 parser.add_argument('--model', required=True, type=str, help="Txt file with input tuple list")
 parser.add_argument('--tree', required=False, type=str, default="taus", help="Tree name")
 parser.add_argument('--batch-size', required=False, type=int, default=20000, help="Batch size")
+parser.add_argument('--max-n-files', required=False, type=int, default=None, help="Maximum number of files to process")
 args = parser.parse_args()
 
 import os
@@ -74,6 +75,17 @@ input_branches = ['pt', 'eta', 'mass', 'decayMode', 'chargedIsoPtSum', 'neutralI
 all_branches = truth_branches + input_branches + central_tau_id_branches
 match_suffixes = [ 'e', 'mu', 'tau', 'jet' ]
 gen_match_ex_branches = [ 'gen_match_{}'.format(suff) for suff in match_suffixes ]
+e, mu, tau, jet = 0, 1, 2, 3
+input_shape = (len(input_branches), )
+n_outputs = len(gen_match_ex_branches)
+
+class GenMatch:
+    Electron = 1
+    Muon = 2
+    TauElectron = 3
+    TauMuon = 4
+    Tau = 5
+    NoMatch = 6
 
 def load_graph(graph_filename):
     with tf.gfile.GFile(graph_filename, 'rb') as f:
@@ -115,10 +127,10 @@ def VectorizeGenMatch(data, dtype):
     if data.shape[1] != 1:
         raise RuntimeError("Invalid input")
     v_data = np.zeros([data.shape[0], 4], dtype=dtype)
-    v_data[:, 0] = ((data[:, 0] == 1) | (data[:, 0] == 3)).astype(dtype)
-    v_data[:, 1] = ((data[:, 0] == 2) | (data[:, 0] == 4)).astype(dtype)
-    v_data[:, 2] = (data[:, 0] == 5).astype(dtype)
-    v_data[:, 3] = (data[:, 0] == 6).astype(dtype)
+    v_data[:, e] = ((data[:, 0] == GenMatch.Electron) | (data[:, 0] == GenMatch.TauElectron)).astype(dtype)
+    v_data[:, mu] = ((data[:, 0] == GenMatch.Muon) | (data[:, 0] == GenMatch.TauMuon)).astype(dtype)
+    v_data[:, tau] = (data[:, 0] == GenMatch.Tau).astype(dtype)
+    v_data[:, jet] = (data[:, 0] == GenMatch.NoMatch).astype(dtype)
     return v_data
 
 def ExtractMuonDiscriminators(id_flags):
@@ -140,17 +152,19 @@ def ProcessFile(session, graph, file_name):
     df = ReadBrancesToDataFrame(full_name, args.tree, result_branches)
     refId_mu = ExtractMuonDiscriminators(df.id_flags)
 
-    X = ReadBranchesTo2DArray(full_name, args.tree, input_branches, np.float32)
+    X = ReadBranchesTo2DArray(full_name, args.tree, input_branches, np.float32, chunk_size=200)
     Y_raw = ReadBranchesTo2DArray(full_name, args.tree, truth_branches, int)
     Y = VectorizeGenMatch(Y_raw, int)
     N = X.shape[0]
 
     print("Running predictions...")
-    x_gr = graph.get_tensor_by_name('deepTau/dense_94_input:0')
-    y_gr = graph.get_tensor_by_name('deepTau/output_node0:0')
+    x_gr_name = graph.get_operations()[0].name
+    y_gr_name = graph.get_operations()[-1].name
+    x_gr = graph.get_tensor_by_name(x_gr_name + ':0')
+    y_gr = graph.get_tensor_by_name(y_gr_name + ':0')
     deepId = np.zeros([N, 4])
 
-    with tqdm(total=N, unit='tau') as pbar:
+    with tqdm(total=N, unit='taus') as pbar:
         for n in range(0, N, args.batch_size):
             pred = session.run(y_gr, feed_dict={x_gr: X[n:n+args.batch_size, :]})
             deepId[n:n+args.batch_size, :] = pred[:, :]
@@ -160,14 +174,21 @@ def ProcessFile(session, graph, file_name):
     return pandas.DataFrame(data = {
         'run': df.run, 'lumi': df.lumi, 'evt': df.evt, 'tau_index': df.tau_index,
         'pt': df.pt, 'eta': df.eta, 'phi': df.phi, 'decayMode': df.decayMode,
-        'gen_e': Y[:, 0], 'gen_mu': Y[:, 1], 'gen_tau': Y[:, 2], 'gen_jet': Y[:, 3],
-        'deepId_e': deepId[:, 0], 'deepId_mu': deepId[:, 1], 'deepId_tau': deepId[:, 2], 'deepId_jet': deepId[:, 3],
+        'gen_e': Y[:, e], 'gen_mu': Y[:, mu], 'gen_tau': Y[:, tau], 'gen_jet': Y[:, jet],
+        'deepId_e': deepId[:, e], 'deepId_mu': deepId[:, mu], 'deepId_tau': deepId[:, tau],
+        'deepId_jet': deepId[:, jet],
         'refId_e': df.againstElectronMVA6Raw, 'refId_mu_loose': refId_mu[:, 0], 'refId_mu_tight': refId_mu[:, 1],
         'refId_jet': df.byIsolationMVArun2017v2DBoldDMwLTraw2017
     })
 
-with open(args.filelist, 'r') as f_list:
-    file_list = [ f.strip() for f in f_list if len(f) != 0 ]
+if args.filelist is None:
+    file_list = [ f for f in os.listdir(args.input) if f.endswith('.root') ]
+else:
+    with open(args.filelist, 'r') as f_list:
+        file_list = [ f.strip() for f in f_list if len(f) != 0 ]
+
+if args.max_n_files is not None and args.max_n_files > 0:
+    file_list = file_list[0:args.max_n_files]
 
 if os.path.isfile(args.output):
     os.remove(args.output)
--- Analysis/python/keras_to_tensorflow.py
+++ Analysis/python/keras_to_tensorflow.py
@@ -5,12 +5,12 @@
 
 """
 Copyright (c) 2017, by the Authors: Amir H. Abdi
-This software is freely available under the MIT Public License. 
+This software is freely available under the MIT Public License.
 Please see the License file in the root for details.
 
 The following code snippet will convert the keras model file,
 which is saved using model.save('kerasmodel_weight_file'),
-to the freezed .pb tensorflow weight file which holds both the 
+to the freezed .pb tensorflow weight file which holds both the
 network architecture and its associated weights.
 """;
 
@@ -20,8 +20,8 @@ network architecture and its associated weights.
 '''
 Input arguments:
 
-num_output: this value has nothing to do with the number of classes, batch_size, etc., 
-and it is mostly equal to 1. If the network is a **multi-stream network** 
+num_output: this value has nothing to do with the number of classes, batch_size, etc.,
+and it is mostly equal to 1. If the network is a **multi-stream network**
 (forked network with multiple outputs), set the value to the number of outputs.
 
 quantize: if set to True, use the quantize feature of Tensorflow
@@ -41,7 +41,7 @@ output_model_file: name of the output weight file [default: args.input_model_fil
 
 graph_def: if set to True, will write the graph definition as an ascii file [default: False]
 
-output_graphdef_file: if graph_def is set to True, the file name of the 
+output_graphdef_file: if graph_def is set to True, the file name of the
 graph definition [default: model.ascii]
 
 output_node_prefix: the prefix to use for output nodes. [default: output_node]
@@ -55,25 +55,25 @@ output_node_prefix: the prefix to use for output nodes. [default: output_node]
 
 import argparse
 parser = argparse.ArgumentParser(description='set input arguments')
-parser.add_argument('-input_fld', action="store", 
+parser.add_argument('-input_fld', action="store",
                     dest='input_fld', type=str, default='.')
-parser.add_argument('-output_fld', action="store", 
+parser.add_argument('-output_fld', action="store",
                     dest='output_fld', type=str, default='')
-parser.add_argument('-input_model_file', action="store", 
+parser.add_argument('-input_model_file', action="store",
                     dest='input_model_file', type=str, default='model.h5')
-parser.add_argument('-output_model_file', action="store", 
+parser.add_argument('-output_model_file', action="store",
                     dest='output_model_file', type=str, default='')
-parser.add_argument('-output_graphdef_file', action="store", 
+parser.add_argument('-output_graphdef_file', action="store",
                     dest='output_graphdef_file', type=str, default='model.ascii')
-parser.add_argument('-num_outputs', action="store", 
+parser.add_argument('-num_outputs', action="store",
                     dest='num_outputs', type=int, default=1)
-parser.add_argument('-graph_def', action="store", 
+parser.add_argument('-graph_def', action="store",
                     dest='graph_def', type=bool, default=False)
-parser.add_argument('-output_node_prefix', action="store", 
+parser.add_argument('-output_node_prefix', action="store",
                     dest='output_node_prefix', type=str, default='output_node')
-parser.add_argument('-quantize', action="store", 
+parser.add_argument('-quantize', action="store",
                     dest='quantize', type=bool, default=False)
-parser.add_argument('-theano_backend', action="store", 
+parser.add_argument('-theano_backend', action="store",
                     dest='theano_backend', type=bool, default=False)
 parser.add_argument('-f')
 args = parser.parse_args()
@@ -96,13 +96,40 @@ from keras import backend as K
 output_fld =  args.input_fld if args.output_fld == '' else args.output_fld
 if args.output_model_file == '':
     args.output_model_file = str(Path(args.input_model_file).name) + '.pb'
-Path(output_fld).mkdir(parents=True, exist_ok=True)    
+Path(output_fld).mkdir(parents=True, exist_ok=True)
 weight_file_path = str(Path(args.input_fld) / args.input_model_file)
 
 
 # Load keras model and rename output
 
 # In[ ]:
+
+e, mu, tau, jet = 0, 1, 2, 3
+def tau_e_loss(target, output):
+    sf = tf.convert_to_tensor(3, output.dtype.base_dtype)
+    epsilon = tf.convert_to_tensor(1e-7, output.dtype.base_dtype)
+    tau_vs_e = output[:, tau] / (output[:, tau] + output[:, e] + epsilon)
+    loss_tau_e = target[:, tau] * tf.log(tau_vs_e) + target[:, e] * tf.log(1 - tau_vs_e)
+    return -sf * loss_tau_e
+
+def tau_mu_loss(target, output):
+    sf = tf.convert_to_tensor(20, output.dtype.base_dtype)
+    epsilon = tf.convert_to_tensor(1e-7, output.dtype.base_dtype)
+    tau_vs_mu = output[:, tau] / (output[:, tau] + output[:, mu] + epsilon)
+    tau_vs_mu = tf.clip_by_value(tau_vs_mu, epsilon, 1 - epsilon)
+    loss_tau_mu = target[:, tau] * tf.log(tau_vs_mu) + target[:, mu] * tf.log(1 - tau_vs_mu)
+    return -sf * loss_tau_mu
+
+def tau_jet_loss(target, output):
+    sf = tf.convert_to_tensor(1, output.dtype.base_dtype)
+    epsilon = tf.convert_to_tensor(1e-7, output.dtype.base_dtype)
+    tau_vs_jet = output[:, tau] / (output[:, tau] + output[:, jet] + epsilon)
+    tau_vs_jet = tf.clip_by_value(tau_vs_jet, epsilon, 1 - epsilon)
+    loss_tau_jet = target[:, tau] * tf.log(tau_vs_jet) + target[:, jet] * tf.log(1 - tau_vs_jet)
+    return -sf * loss_tau_jet
+
+def tau_crossentropy(target, output):
+    return tau_e_loss(target, output) + tau_mu_loss(target, output) + tau_jet_loss(target, output)
 
 K.set_learning_phase(0)
 if args.theano_backend:
@@ -111,12 +138,14 @@ else:
     K.set_image_data_format('channels_last')
 
 try:
-    net_model = load_model(weight_file_path)
+    net_model = load_model(weight_file_path, custom_objects = {
+        'tau_crossentropy': tau_crossentropy,
+        'tau_e_loss': tau_e_loss, 'tau_mu_loss': tau_mu_loss, 'tau_jet_loss': tau_jet_loss })
 except ValueError as err:
     print('''Input file specified ({}) only holds the weights, and not the model defenition.
     Save the model using mode.save(filename.h5) which will contain the network architecture
-    as well as its weights. 
-    If the model is saved using model.save_weights(filename.h5), the model architecture is 
+    as well as its weights.
+    If the model is saved using model.save_weights(filename.h5), the model architecture is
     expected to be saved separately in a json format and loaded prior to loading the weights.
     Check the keras documentation for more details (https://keras.io/getting-started/faq/)'''
           .format(weight_file_path))
@@ -137,7 +166,7 @@ print('output nodes names are: ', pred_node_names)
 sess = K.get_session()
 
 if args.graph_def:
-    f = args.output_graphdef_file 
+    f = args.output_graphdef_file
     tf.train.write_graph(sess.graph.as_graph_def(), output_fld, f, as_text=True)
     print('saved the graph definition in ascii format at: ', str(Path(output_fld) / f))
 
@@ -154,7 +183,6 @@ if args.quantize:
     transformed_graph_def = TransformGraph(sess.graph.as_graph_def(), [], pred_node_names, transforms)
     constant_graph = graph_util.convert_variables_to_constants(sess, transformed_graph_def, pred_node_names)
 else:
-    constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)    
+    constant_graph = graph_util.convert_variables_to_constants(sess, sess.graph.as_graph_def(), pred_node_names)
 graph_io.write_graph(constant_graph, output_fld, args.output_model_file, as_text=False)
 print('saved the freezed graph (ready for inference) at: ', str(Path(output_fld) / args.output_model_file))
-
