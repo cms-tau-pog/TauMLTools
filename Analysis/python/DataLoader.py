166084629cb4aad037e1a12a4686515f1f13c021
Konstantin Androsov <konstantin.androsov@gmail.com>
2019-02-27 10:50:27 +0100

    Bugfixes.
    Former-commit-id: e1d7f13690965ecadfddb3eab12520db9fbd274a

--- Analysis/python/DataLoader.py
+++ Analysis/python/DataLoader.py
@@ -1,5 +1,6 @@
 import glob
 import math
+import gc
 from concurrent.futures import ThreadPoolExecutor
 from queue import Queue
 from threading import Thread
@@ -18,7 +19,9 @@ def LoaderThread(file_name, queue, executor, batch_size, chunk_size, tau_begin, 
                            input_cell_common_branches + input_cell_ele_branches,
                            input_cell_common_branches + input_cell_muon_branches ]
 
+    expected_n_batches = math.ceil((tau_end - tau_begin) / batch_size)
     tau_current = tau_begin
+    global_batch_id = 0
     while tau_current < tau_end:
         entry_stop = min(tau_current + chunk_size, tau_end)
         df_taus = taus_tree.arrays(branches=df_tau_branches, outputtype=pandas.DataFrame, namedecode='utf-8',
@@ -33,16 +36,20 @@ def LoaderThread(file_name, queue, executor, batch_size, chunk_size, tau_begin, 
                                                    namedecode='utf-8', entrystart=cells_begin,
                                                    entrystop=cells_end, executor=executor)
             cells_begin_ref[loc] = cells_begin
-        n_batches = math.ceil(chunk_size / float(batch_size))
+        current_chunk_size = entry_stop - tau_current
+        n_batches = math.ceil(current_chunk_size / float(batch_size))
         for batch_id in range(n_batches):
+            if global_batch_id >= expected_n_batches:
+                raise RuntimeError("Too many batches")
+            global_batch_id += 1
             b_tau_begin = batch_id * batch_size
             b_tau_end = min(df_taus.shape[0], (batch_id + 1) * batch_size)
             b_size = b_tau_end - b_tau_begin
 
-            X_taus = np.empty((b_size, len(input_tau_branches)))
-            Y = np.empty((b_size, n_outputs), dtype=int)
+            X_taus = np.empty((b_size, len(input_tau_branches)), dtype=np.float32)
+            Y = np.empty((b_size, n_outputs), dtype=np.int)
             if return_weights:
-                weights = np.empty(b_size)
+                weights = np.empty(b_size, dtype=np.float32)
 
             X_taus[:, :] = df_taus[input_tau_branches].values[b_tau_begin:b_tau_end, :]
             Y[:, :] = df_taus[truth_branches].values[b_tau_begin:b_tau_end, :]
@@ -57,9 +64,8 @@ def LoaderThread(file_name, queue, executor, batch_size, chunk_size, tau_begin, 
                 b_cells_begin = b_cells_begins[0] - cells_begin_ref[loc]
                 b_cells_end = b_cells_ends[-1] - cells_begin_ref[loc]
                 for cmp_branches in component_branches:
-                    X_cells_comp = np.zeros((b_size, n_cells_eta[loc], n_cells_phi[loc], len(cmp_branches)))
-                    FillGrid(b_cells_begins, b_cells_ends, (n_cells_eta[loc] - 1) / 2, (n_cells_phi[loc] - 1) / 2,
-                             df_cells[loc][cmp_branches].values[b_cells_begin:b_cells_end, :], X_cells_comp)
+                    X_cells_comp = FillGrid(b_cells_begins, b_cells_ends, n_cells_eta[loc], n_cells_phi[loc],
+                                            df_cells[loc][cmp_branches].values[b_cells_begin:b_cells_end, :])
                     X_all.append(X_cells_comp)
 
             if return_truth and return_weights:
@@ -71,6 +77,11 @@ def LoaderThread(file_name, queue, executor, batch_size, chunk_size, tau_begin, 
             else:
                 item = X_all
             queue.put(item)
+        tau_current = entry_stop
+        del df_taus
+        for loc in cell_locations:
+            del df_cells[loc]
+        gc.collect()
 
 class DataLoader:
     @staticmethod
@@ -79,13 +90,15 @@ class DataLoader:
             tree = file[tree_name]
             return tree.numentries
 
-    def __init__(self, file_name, batch_size, chunk_size, validation_size = None, n_passes = -1):
+    def __init__(self, file_name, batch_size, chunk_size, validation_size = None, max_data_size = None,
+                 max_queue_size = 4, n_passes = -1):
         if batch_size <= 0 or chunk_size <= 0:
             raise RuntimeError("batch_size and chunk_size should be positive numbers")
         self.file_name = file_name
         self.batch_size = batch_size
         self.chunk_size = chunk_size
         self.n_passes = n_passes
+        self.max_queue_size = max_queue_size
         self.file_size = DataLoader.GetNumberOfEntries(self.file_name, 'taus')
         self.has_validation_set = validation_size is not None
 
@@ -99,6 +112,9 @@ class DataLoader:
             self.first_tau_index = 0
             self.data_size = self.file_size
 
+        if max_data_size is not None:
+            self.data_size = min(self.data_size, max_data_size)
+
         self.steps_per_epoch = math.ceil(self.data_size / self.batch_size)
         if self.has_validation_set:
             self.validation_steps = math.ceil(self.validation_size / self.batch_size)
@@ -107,14 +123,14 @@ class DataLoader:
     def generator(self, primary_set = True, return_truth = True, return_weights = True):
         if primary_set:
             tau_begin = self.first_tau_index
-            tau_end = self.file_size
+            tau_end = self.first_tau_index + self.data_size
             steps_per_epoch = self.steps_per_epoch
         else:
             tau_begin = 0
             tau_end = self.validation_size
             steps_per_epoch = self.validation_steps
 
-        queue = Queue(maxsize=10)
+        queue = Queue(maxsize=self.max_queue_size)
         current_pass = 0
         while self.n_passes < 0 or current_pass < self.n_passes:
             thread = Thread(target=LoaderThread,
@@ -125,5 +141,6 @@ class DataLoader:
             for step_id in range(steps_per_epoch):
                 item = queue.get()
                 yield item
-
+            thread.join()
+            gc.collect()
             current_pass += 1
--- Analysis/python/fill_grid.pyx
+++ Analysis/python/fill_grid.pyx
@@ -2,8 +2,12 @@ cimport numpy as np
 import numpy as np
 import math
 
-cpdef void FillGrid(np.ndarray cells_begin, np.ndarray cells_end, int max_eta_index, int max_phi_index,
-                    np.ndarray packed_cells, np.ndarray unpacked_cells):
+cpdef np.ndarray[float, ndim=4] FillGrid(np.ndarray cells_begin, np.ndarray cells_end, int n_cells_eta,
+                                          int n_cells_phi, np.ndarray packed_cells):
+    cdef np.ndarray[float, ndim=4] unpacked_cells = np.zeros((cells_begin.shape[0], n_cells_eta, n_cells_phi,
+                                                              packed_cells.shape[1]), dtype=np.float32)
+    cdef int max_eta_index = (n_cells_eta - 1) / 2
+    cdef int max_phi_index = (n_cells_phi - 1) / 2
     begin_ref = cells_begin[0]
     for tau_index in range(cells_begin.shape[0]):
         begin = cells_begin[tau_index] - begin_ref
@@ -12,3 +16,4 @@ cpdef void FillGrid(np.ndarray cells_begin, np.ndarray cells_end, int max_eta_in
             eta_index = int(packed_cells[cell_index, 0] + max_eta_index)
             phi_index = int(packed_cells[cell_index, 1] + max_phi_index)
             unpacked_cells[tau_index, eta_index, phi_index, :] = packed_cells[cell_index, :]
+    return unpacked_cells
