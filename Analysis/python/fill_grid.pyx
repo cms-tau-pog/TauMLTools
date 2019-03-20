7b0dad7bd3e183d4f330cefd9286a2db24f72cee
Konstantin Androsov <konstantin.androsov@gmail.com>
2019-03-20 14:15:34 +0100

    working on Training 2017v2p3
    Former-commit-id: eea39c2579e691a83a31cc8b73def2793fe76964

--- Analysis/python/fill_grid.pyx
+++ Analysis/python/fill_grid.pyx
@@ -3,17 +3,21 @@ import numpy as np
 import math
 
 cpdef np.ndarray[float, ndim=4] FillGrid(np.ndarray cells_begin, np.ndarray cells_end, int n_cells_eta,
-                                          int n_cells_phi, np.ndarray packed_cells):
-    cdef np.ndarray[float, ndim=4] unpacked_cells = np.zeros((cells_begin.shape[0], n_cells_eta, n_cells_phi,
-                                                              packed_cells.shape[1]), dtype=np.float32)
+                                         int n_cells_phi, np.ndarray cell_indices, np.ndarray packed_cells,
+                                         np.ndarray other_inputs):
+    cdef np.ndarray[float, ndim=4] unpacked_cells = np.zeros(
+        ( cells_begin.shape[0], n_cells_eta, n_cells_phi, other_inputs.shape[1] + packed_cells.shape[1] ),
+        dtype=np.float32)
     cdef int max_eta_index = (n_cells_eta - 1) / 2
     cdef int max_phi_index = (n_cells_phi - 1) / 2
+    cdef int n_other_inputs = other_inputs.shape[1]
     begin_ref = cells_begin[0]
     for tau_index in range(cells_begin.shape[0]):
         begin = cells_begin[tau_index] - begin_ref
         end = cells_end[tau_index] - begin_ref
-        for cell_index in range(begin, end):
-            eta_index = int(packed_cells[cell_index, 0] + max_eta_index)
-            phi_index = int(packed_cells[cell_index, 1] + max_phi_index)
-            unpacked_cells[tau_index, eta_index, phi_index, :] = packed_cells[cell_index, :]
+        for flat_cell_index in range(begin, end):
+            eta_index = cell_indices[flat_cell_index, 0] + max_eta_index
+            phi_index = cell_indices[flat_cell_index, 1] + max_phi_index
+            unpacked_cells[tau_index, eta_index, phi_index, 0:n_other_inputs] = other_inputs[tau_index, :]
+            unpacked_cells[tau_index, eta_index, phi_index, n_other_inputs:] = packed_cells[flat_cell_index, :]
     return unpacked_cells
