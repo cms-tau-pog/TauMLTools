88b939b810d0f3e8e03ecefa772b5bcba925864d
shedprog <flynik75@gmail.com>
2020-08-09 18:19:08 +0200

    Root-tuples production: crab scripts update

--- Production/crab/submit.py
+++ Production/crab/submit.py
@@ -9,20 +9,26 @@ from sets import Set
 
 parser = argparse.ArgumentParser(description='Submit jobs on CRAB.',
                   formatter_class = lambda prog: argparse.HelpFormatter(prog,width=90))
-parser.add_argument('--workArea', required=False, default="work_area", help="Work area")
-parser.add_argument('--cfg', required=True, help="CMSSW configuration file")
+parser.add_argument('--workArea', required=True, type=str, default="work_area", help="Work area")
+parser.add_argument('--cfg', required=True, type=str, help="CMSSW configuration file")
 parser.add_argument('--scriptExe', required=False, default="", help="Custom script to execute")
-parser.add_argument('--site', required=True, help="Site for stage out.")
+parser.add_argument('--site', required=True, type=str, help="Site for stage out.")
 parser.add_argument('--dryrun', action="store_true", help="Submission dryrun.")
-parser.add_argument('--output', required=True, help="output path after /store/user/USERNAME")
-parser.add_argument('--blacklist', required=False, default="", help="list of sites where the jobs shouldn't run")
-parser.add_argument('--whitelist', required=False, default="", help="list of sites where the jobs can run")
-parser.add_argument('--jobNames', required=False, default="",
+parser.add_argument('--output', required=True, type=str, help="output path after /store/user/USERNAME")
+parser.add_argument('--blacklist', required=False, type=str, default="",
+					help="list of sites where the jobs shouldn't run")
+parser.add_argument('--whitelist', required=False, type=str, default="",
+					help="list of sites where the jobs can run")
+parser.add_argument('--jobNames', required=False, type=str, default="",
 					help="list of job names to submit (if not specified - submit all)")
-parser.add_argument('--lumiMask', required=False, default="",
+parser.add_argument('--lumiMask', required=False, type=str, default="",
 					help="json file with a lumi mask (default: apply lumi mask from the config file)")
-parser.add_argument('--jobNameSuffix', required=False, default="", help="suffix that will be added to each job name")
-parser.add_argument('--unitsPerJob', required=False, type=int, default=-1,
+parser.add_argument('--jobNameSuffix', required=False, type=str, default="",
+					help="suffix that will be added to each job name")
+parser.add_argument('--inputDBS', required=False, default="global", help="DBS instance")
+parser.add_argument('--splitting', required=False, default="Automatic",
+					help="suffix that will be added to each job name")
+parser.add_argument('--unitsPerJob', required=False, type=int, default=1000,
 					help="number of units per job (default: use values from the config file)")
 parser.add_argument('--maxMemory', required=False, type=int, default=2500,
 					help="maximum amount of memory (in MB) a job is allowed to use (default: 2500 MB )")
@@ -31,7 +37,8 @@ parser.add_argument('--allowNonValid', action="store_true", help="Allow nonvalid
 parser.add_argument('job_file', nargs='+', help="text file with jobs descriptions")
 args = parser.parse_args()
 
-from CRABClient.UserUtilities import config, ClientException, getUsernameFromSiteDB
+from CRABClient.UserUtilities import config, ClientException
+from CRABClient.UserUtilities import getUsernameFromCRIC as getUsername
 from CRABAPI.RawCommand import crabCommand
 from httplib import HTTPException
 
@@ -46,7 +53,7 @@ config.JobType.numCores = args.numCores
 if len(args.scriptExe) > 0:
     config.JobType.scriptExe = args.scriptExe
 
-config.Data.inputDBS = 'global'
+config.Data.inputDBS = args.inputDBS
 config.Data.allowNonValidInputDataset = args.allowNonValid
 config.General.transferOutputs = True
 config.General.transferLogs = True
@@ -54,7 +61,7 @@ config.Data.publication = False
 
 
 config.Site.storageSite = args.site
-config.Data.outLFNDirBase = "/store/user/{}/{}".format(getUsernameFromSiteDB(), args.output)
+config.Data.outLFNDirBase = "/store/user/{}/{}".format(getUsername(), args.output)
 
 if len(args.blacklist) != 0:
 	config.Site.blacklist = re.split(',', args.blacklist)
@@ -66,10 +73,12 @@ job_names = Set(filter(lambda s: len(s) != 0, re.split(",", args.jobNames)))
 from crab_tools import JobCollection
 try:
     for job_file in args.job_file:
-        job_collection = JobCollection(job_file, job_names, args.lumiMask, args.jobNameSuffix, args.unitsPerJob)
+        job_collection = JobCollection(job_file, job_names, args.lumiMask, args.jobNameSuffix)
         print job_file
         print job_collection
-        job_collection.submit(config,args.dryrun)
+    	print "Splitting: {} with {} units per job".format(args.splitting, args.unitsPerJob)
+	job_collection.submit(config, args.splitting, args.unitsPerJob ,args.dryrun)
+
 except RuntimeError as err:
     print >> sys.stderr, "ERROR:", str(err)
     sys.exit(1)
