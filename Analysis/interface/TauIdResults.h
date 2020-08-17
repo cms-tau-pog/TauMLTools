13e950effe301580b99e2f63244b97b6b5c658ee
Artur Gottmann <artur.gottmann@kit.edu>
2020-08-17 20:08:33 +0200

    Analysis/interface/TauIdResults.h: catch the case, if the required tau ID is not available

--- Analysis/interface/TauIdResults.h
+++ Analysis/interface/TauIdResults.h
@@ -124,11 +124,11 @@ struct TauIdDescriptor {
     {
         const std::string disc_name = ::analysis::ToString(discriminator);
         if(has_raw)
-            tuple.template get<float>(prefix + disc_name + raw_suffix) = tau ? tau->tauID(raw_name) : default_value;
+            tuple.template get<float>(prefix + disc_name + raw_suffix) = tau && tau->isTauIDAvailable(raw_name) ? tau->tauID(raw_name) : default_value;
         if(!working_points.empty()) {
             TauIdResults id_results;
             for(const auto& wp_entry : working_points) {
-                const bool result = tau && tau->tauID(wp_entry.second) > 0.5;
+                const bool result = tau && tau->isTauIDAvailable(wp_entry.second) && tau->tauID(wp_entry.second) > 0.5;
                 id_results.SetResult(wp_entry.first, result);
             }
             tuple.template get<TauIdResults::BitsContainer>(prefix + disc_name) = id_results.GetResultBits();
