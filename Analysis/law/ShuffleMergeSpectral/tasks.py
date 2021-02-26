2e2e3cce8723a48106c10a0d6f245cabc5b2c056
Luca Guzzi <luca.guzzi@cern.ch>
2021-02-26 19:01:01 +0100

    use shutil to move files

--- Analysis/law/ShuffleMergeSpectral/tasks.py
+++ Analysis/law/ShuffleMergeSpectral/tasks.py
@@ -6,6 +6,7 @@ import subprocess
 import os
 import re
 import sys
+import shutil
 
 from framework import Task, HTCondorWorkflow
 import luigi
@@ -91,8 +92,8 @@ class ShuffleMergeSpectral(Task, HTCondorWorkflow, law.LocalWorkflow):
     if retcode != 0:
       raise Exception('job {} return code is {}'.format(self.branch, retcode))
     elif retcode == 0 and self.mode == 'MergeAll':
-      os.rename(output_name, '/'.join([self.output_dir, '..', file_name]))
-      os.rename('./out', '/'.join([self.output_dir, '..', 'hashes', 'out_{}'.format(self.branch)]))
+      shutil.move(output_name, '/'.join([self.output_dir, '..', file_name]))
+      shutil.move('./out', '/'.join([self.output_dir, '..', 'hashes', 'out_{}'.format(self.branch)]))
       taskout = self.output()
       taskout.dump('Task ended with code %s\n' %retcode)
     else:
