5a815aa3c0e9c33173a0b4722f93eaa2f30fef81
yaourtpourtoi <oleg.filatov@phystech.edu>
2021-05-14 15:10:12 +0200

    documenting the code

--- Training/python/feature_scaling.py
+++ Training/python/feature_scaling.py
@@ -15,12 +15,14 @@ from scaling_utils import Phi_mpi_pi, dR, dR_signal_cone, compute_mean, compute_
 from scaling_utils import nested_dict, init_dictionaries, dump_to_json
 
 if __name__ == '__main__':
+    # parse command line parameters
     parser = argparse.ArgumentParser()
     parser.add_argument("--cfg", type=str, help="Path to yaml configuration file")
-    parser.add_argument('--var_types', nargs='+', help="Variable types for which to derive scaling constants. Defaults to -1 for running on all those specified in the config", default=-1)
+    parser.add_argument('--var_types', nargs='+', help="Variable types from field 'Features_all' of the cfg file for which to derive scaling parameters. Defaults to -1 for running on all those specified in the cfg", default=-1)
     args = parser.parse_args()
     with open(args.cfg) as f:
         scaling_dict = yaml.load(f, Loader=yaml.FullLoader)
+
     # read cfg parameters
     setup_dict = scaling_dict['Scaling_setup']
     features_dict = scaling_dict['Features_all']
@@ -45,7 +47,6 @@ if __name__ == '__main__':
     assert log_step > 0 and type(log_step) == int
     assert len(file_range)==2 and file_range[0]<=file_range[1]
     file_names = sorted(glob(file_path))[file_range[0]:file_range[1]]
-    sums, sums2, counts, scaling_params = init_dictionaries(features_dict, cone_selection_dict, len(file_names))
     #
     dR_tau_outer_cone = cone_definition_dict['outer']['dR']
     tau_pt_name, tau_eta_name, tau_phi_name = cone_selection_dict['TauFlat']['var_names']['pt'], cone_selection_dict['TauFlat']['var_names']['eta'], cone_selection_dict['TauFlat']['var_names']['phi']
@@ -53,12 +54,16 @@ if __name__ == '__main__':
     inner_cone_min_radius = cone_definition_dict['inner']['min_radius']
     inner_cone_opening_coef = cone_definition_dict['inner']['opening_coef']
     #
+    # initialise dictionaries to be filled
+    sums, sums2, counts, scaling_params = init_dictionaries(features_dict, cone_selection_dict, len(file_names))
+    #
     print(f'\n[INFO] will process {len(file_names)} input files from {file_path}')
     print(f'[INFO] will dump scaling parameters to {scaling_params_json_prefix}_*.json after every {log_step} files')
     print('[INFO] starting to accumulate sums & counts:\n')
     #
     skip_counter = 0
     processed_last_file = time.time()
+
     # loop over input files
     for file_i, file_name in enumerate(tqdm(file_names)):
         log_scaling_params = not (file_i%log_step) or (file_i == len(file_names)-1)
@@ -70,15 +75,18 @@ if __name__ == '__main__':
                 tree = f[tree_name]
                 # NB: selection cut is not applied on tau branches
                 tau_pt_array, tau_eta_array, tau_phi_array = tree.arrays([tau_pt_name, tau_eta_name, tau_phi_name], cut=None, aliases=None, how=tuple)
+                # loop over variable type
                 for var_type in var_types:
+                    # loop over variables of the given type
                     for var_dict in features_dict[var_type]:
                         begin_var = time.time()
                         (var, (selection_cut, aliases, scaling_type, *lim_params)), = var_dict.items()
-                        if scaling_type != 'normal': continue
+                        if scaling_type != 'normal': continue # other scaling_type are already initialised with mean, std and lim_min/lim_max
                         constituent_eta_name, constituent_phi_name = cone_selection_dict[var_type]['var_names']['eta'], cone_selection_dict[var_type]['var_names']['phi']
                         # NB: selection cut is applied, broadcasting with tau array (w/o cut) correctly handles the difference
                         var_array, constituent_eta_array, constituent_phi_array = tree.arrays([var, constituent_eta_name, constituent_phi_name], cut=selection_cut, aliases=aliases, how=tuple)
                         dR_tau_signal_cone = dR_signal_cone(tau_pt_array, inner_cone_min_pt, inner_cone_min_radius, inner_cone_opening_coef)
+                        # loop over cone types specified for a given var_type in the cfg file
                         for cone_type in cone_selection_dict[var_type]['cone_types']:
                             fill_aggregators(var_array, tau_eta_array, tau_phi_array, constituent_eta_array, constituent_phi_array,
                                              var, var_type, file_i, cone_type, dR_tau_signal_cone, dR_tau_outer_cone,
@@ -89,6 +97,7 @@ if __name__ == '__main__':
                         # print(f'---> processed {var} in {end_var - begin_var:.2f} s\n')
                 del(tau_pt_array, tau_eta_array, tau_phi_array)
         gc.collect()
+        # snapshot scaling params into json if log_step is reached
         if log_scaling_params:
             if file_i == len(file_names)-1:
                 scaling_params_json_name = scaling_params_json_prefix
