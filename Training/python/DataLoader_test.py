f32e46a581272cff7ee73da48948290d7ef243e4
shedprog <flynik75@gmail.com>
2021-06-22 20:03:36 +0200

    Updated DataLoader_test script

--- Training/python/DataLoader_test.py
+++ Training/python/DataLoader_test.py
@@ -28,28 +28,54 @@ n_ele       = R.Setup.n_Electron
 n_muon      = R.Setup.n_Muon
 tau_types   = R.Setup.tau_types_names.size()
 
+n_grid_features = {
+    "PfCand_electron" : n_pf_el,
+    "PfCand_muon" : n_pf_mu,
+    "PfCand_chHad" : n_pf_chHad,
+    "PfCand_nHad" : n_pf_nHad,
+    "PfCand_gamma" : n_pf_gamma,
+    "Electron" : n_ele,
+    "Muon" : n_muon
+}
+
+input_grids =[ [ "PfCand_electron", "PfCand_gamma", "Electron" ], [ "PfCand_muon", "Muon" ], [ "PfCand_chHad", "PfCand_nHad" ] ]
+
 input_files = []
 for root, dirs, files in os.walk(os.path.abspath(R.Setup.input_dir)):
     for file in files:
         input_files.append(os.path.join(root, file))
 
-def ListToVector(l, elem_type):
-    vec = R.std.vector(elem_type)()
-    for elem in l:
-        vec.push_back(elem)
-    return vec
-
-data_loader = R.DataLoader(ListToVector(input_files,'string'))
-print("Number of all entries / batch size: ", data_loader.GetEntries()/n_tau)
+data_loader = R.DataLoader()
 
 n_batches = 1000
 n_batches_store = 5
-
 
 from queue import Queue
 data_que = Queue(maxsize = n_batches_store)
 
 times = []
+
+def getdata(_obj_f, _reshape, _dtype=np.float32):
+    x = np.copy(np.frombuffer(_obj_f.data(), dtype=_dtype, count=_obj_f.size()))
+    return x if _reshape==-1 else x.reshape(_reshape)
+
+def getgrid(_obj_grid, _inner):
+    _n_cells = n_inner_cells if _inner else n_outer_cells
+    _X = []
+    for group in input_grids:
+        _X.append(
+            np.concatenate(
+                [ getdata(_obj_grid[ getattr(R.CellObjectType,fname) ][_inner],
+                    (n_tau, _n_cells, _n_cells, n_grid_features[fname])) for fname in group ],
+                axis=-1
+                )
+            )
+    return _X
+
+c = 0
+data_loader.ReadFile(R.std.string(input_files[c]), 0, -1)
+c+=1
+
 for i in range(n_batches):
 
     start = time.time()
@@ -57,28 +83,27 @@ for i in range(n_batches):
     if(data_que.full()):
         _ = data_que.get()
 
-    data_loader.MoveNext()
+    checker = data_loader.MoveNext()
+
+    if checker==False:
+       data_loader.ReadFile(R.std.string(input_files[c]), 0, -1)
+       c+=1
+       continue
+
     data = data_loader.LoadData()
 
-    X = [
-        np.copy(np.frombuffer(data.y_onehot.data(), dtype=np.float32, count=data.y_onehot.size())).reshape((n_tau, tau_types)),
-        np.copy(np.frombuffer(data.weight.data(), dtype=np.float32, count=data.weight.size())),
-        np.copy(np.frombuffer(data.x_tau.data(), dtype=np.float32, count=data.x_tau.size())).reshape((n_tau,n_fe_tau)),
-        np.copy(np.frombuffer(data.x_grid[0][0].data(), dtype=np.float32, count=data.x_grid[0][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_pf_el)),
-        np.copy(np.frombuffer(data.x_grid[1][0].data(), dtype=np.float32, count=data.x_grid[1][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_pf_mu)),
-        np.copy(np.frombuffer(data.x_grid[2][0].data(), dtype=np.float32, count=data.x_grid[2][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_pf_chHad)),
-        np.copy(np.frombuffer(data.x_grid[3][0].data(), dtype=np.float32, count=data.x_grid[3][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_pf_nHad)),
-        np.copy(np.frombuffer(data.x_grid[4][0].data(), dtype=np.float32, count=data.x_grid[4][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_pf_gamma)),
-        np.copy(np.frombuffer(data.x_grid[5][0].data(), dtype=np.float32, count=data.x_grid[5][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_ele)),
-        np.copy(np.frombuffer(data.x_grid[6][0].data(), dtype=np.float32, count=data.x_grid[6][0].size())).reshape((n_tau, n_outer_cells, n_outer_cells, n_muon)),
-        np.copy(np.frombuffer(data.x_grid[0][1].data(), dtype=np.float32, count=data.x_grid[0][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_pf_el)),
-        np.copy(np.frombuffer(data.x_grid[1][1].data(), dtype=np.float32, count=data.x_grid[1][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_pf_mu)),
-        np.copy(np.frombuffer(data.x_grid[2][1].data(), dtype=np.float32, count=data.x_grid[2][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_pf_chHad)),
-        np.copy(np.frombuffer(data.x_grid[3][1].data(), dtype=np.float32, count=data.x_grid[3][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_pf_nHad)),
-        np.copy(np.frombuffer(data.x_grid[4][1].data(), dtype=np.float32, count=data.x_grid[4][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_pf_gamma)),
-        np.copy(np.frombuffer(data.x_grid[5][1].data(), dtype=np.float32, count=data.x_grid[5][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_ele)),
-        np.copy(np.frombuffer(data.x_grid[6][1].data(), dtype=np.float32, count=data.x_grid[6][1].size())).reshape((n_tau, n_inner_cells, n_inner_cells, n_muon))
-    ]
+
+    # Flat Tau features
+    X = [getdata(data.x_tau, (n_tau, n_fe_tau))]
+    # Inner grid
+    X += getgrid(data.x_grid, 1) # 500 11 11 176
+    # Outer grid
+    X += getgrid(data.x_grid, 0) # 500 21 21 176
+
+    X = tuple(X)
+
+    weights = getdata(data.weight, -1)
+    Y = getdata(data.y_onehot, (n_tau, tau_types))
 
     data_que.put(X)
 
@@ -86,15 +111,14 @@ for i in range(n_batches):
     print(i, " end: ",end-start, ' s.')
     times.append(end-start)
 
-    for n in range(17):
-        if np.isnan(data_que.queue[0][1][n]).any():
-            print("Nan detected! element=",n)
-            print(np.argwhere(np.isnan(data_que.queue[0][1][n])))
-        if np.isinf(data_que.queue[0][1][n]).any():
-            print("Inf detected! element=",n)
-            print(np.argwhere(np.isinf(data_que.queue[0][1][n])))
-        if np.amax(data_que.queue[0][1][n])==0:
-            print("Empty tuple detected! element=",n)
+    for x in X:
+        if np.isnan(x).any():
+            print("Nan detected! element=",x.shape) 
+            print(np.argwhere(x))
+        if np.isinf(x).any():
+            print("Inf detected! element=",x.shape)
+        if np.amax(x)==0:
+            print("Empty tuple detected! element=",x.shape)
 
 from statistics import mean
 print("Mean time: ", mean(times))
