0c7b50f4c863d21237393af202b15a4411cb7fd0
shedprog <flynik75@gmail.com>
2021-07-21 16:12:21 +0200

    Testing script for dataloader

--- Training/python/2018v1/Training_test.py
+++ Training/python/2018v1/Training_test.py
@@ -11,20 +11,27 @@ scaling  = os.path.abspath("../../configs/scaling_params_v1.json")
 dataloader = DataLoader.DataLoader(config, scaling)
 
 gen_train = dataloader.get_generator(primary_set = True)
-gen_val = dataloader.get_generator(primary_set = False)
+# gen_val = dataloader.get_generator(primary_set = False)
 
-data_train = tf.data.Dataset.from_generator(gen_train, output_types = input_types, output_shapes = input_shape)
-data_val = tf.data.Dataset.from_generator(gen_val, output_types = input_types, output_shapes = input_shape)
+netConf_full, input_shape, input_types  = dataloader.get_config()
+
+data_train = tf.data.Dataset.from_generator(
+    gen_train, output_types = input_types, output_shapes = input_shape
+    ).prefetch(10)
 
 time_checkpoints = [time.time()]
 
-for i,_ in enumerate(gen_train()):
-    time_checkpoints.append(time.time())
-    print(i, " ", time_checkpoints[-1]-time_checkpoints[-2], "s.")
+for epoch in range(3):
+    print("Epoch ->", epoch)
+    for i,_ in enumerate(data_train):
+        # if i % 10 == 0:
+        #     time.sleep(10)
+        time_checkpoints.append(time.time())
+        print(i, " ", time_checkpoints[-1]-time_checkpoints[-2], "s.")
     
-for i,_ in enumerate(gen_train()):
-    time_checkpoints.append(time.time())
-    print(i, " ", time_checkpoints[-1]-time_checkpoints[-2], "s.")
+# for i,_ in enumerate(gen_train()):
+#     time_checkpoints.append(time.time())
+#     print(i, " ", time_checkpoints[-1]-time_checkpoints[-2], "s.")
 
 
 
