901912f150ad15f79d8e069630834b94fbb0f0cc
Oleg Filatov <oleg.filatov@phystech.edu>
2021-08-19 08:28:09 +0200

    wip adaptation of eval_perf.py for new json format

--- Training/python/evaluate_performance.py
+++ Training/python/evaluate_performance.py
@@ -31,6 +31,7 @@ import pandas
 import numpy as np
 import json
 import eval_tools
+from collections import defaultdict
 
 def AddPredictionsToDataFrame(df, file_name, label = ''):
     df_pred = pandas.read_hdf(file_name)
@@ -95,8 +96,8 @@ for setup_arg in setup_args_list:
 
 setup_provider.Initialize(eval_tools, setup_args)
 
-discriminators = setup_provider.GetDiscriminators(args.other_type, args.deep_results_label,
-                                                  args.prev_deep_results_label)
+discriminator = setup_provider.GetDiscriminators(args.other_type, args.deep_results_label,
+                                                  args.prev_deep_results_label) # wip: here the function is changed to return only one discr
 
 
 match_suffixes = [ 'e', 'mu', 'tau', 'jet' ]
@@ -107,11 +108,11 @@ all_branches = []
 all_branches.extend(core_branches)
 if hasattr(setup_provider, 'setup_branches'):
     all_branches.extend(setup_provider.setup_branches)
-for disc in discriminators:
-    if disc.from_tuple:
-        all_branches.append(disc.column)
-        if disc.wp_column != disc.column:
-            all_branches.append(disc.wp_column)
+
+if discriminator.from_tuple:
+    all_branches.append(discriminator.column)
+    if discriminator.wp_column != discriminator.column:
+        all_branches.append(discriminator.wp_column)
 
 if args.input_other is None:
     df_all = CreateDF(args.input_taus, ['tau', args.other_type], setup_provider)
@@ -121,97 +122,91 @@ else:
     df_all = df_taus.append(df_other)
 if hasattr(setup_provider, 'ApplySelection'):
     df_all = setup_provider.ApplySelection(df_all, args.input_other)
-
-roc_json = []
+    
 plot_setup = setup_provider.GetPlotSetup(args.other_type)
+discr_json = {
+    'name': discriminator.name, 'period': '2017 (13 TeV)', 'metrics': defaultdict(list), 
+}
+
 pt_bins = setup_provider.GetPtBins()
 for pt_index in range(len(pt_bins) - 1):
     df_tx = df_all[(df_all.tau_pt > pt_bins[pt_index]) & (df_all.tau_pt < pt_bins[pt_index + 1])]
     if df_tx.shape[0] == 0:
         print("Warning: pt bin ({}, {}) is empty.".format(pt_bins[pt_index], pt_bins[pt_index + 1]))
         continue
-    n_discr = len(discriminators)
-    rocs = [None] * n_discr
-    wp_rocs = [None] * n_discr
-    names = [ disc.name for disc in discriminators ]
 
-    roc_json_entry = {
-        'pt_min': pt_bins[pt_index], 'pt_max': pt_bins[pt_index + 1], 'discriminators': [], 'plot_setup': { },
-    }
+    # create roc curve and working points
+    roc, wp_roc = discriminator.CreateRocCurve(df_tx)
+    
+    if roc is not None:
+        # prune the curve
+        lim = getattr(plot_setup,  'xlim')
+        x_range = lim[1] - lim[0] if lim is not None else 1
+        roc = roc.Prune(tpr_decimals=max(0, round(math.log10(1000 / x_range))))
+        if roc.auc_score is not None:
+            print('[{}, {}] {} roc_auc = {}'.format(pt_bins[pt_index], pt_bins[pt_index + 1], discriminator.name,
+                                                    roc.auc_score))
 
-    for param_name in [ 'ylabel', 'yscale', 'ratio_yscale', 'legend_loc', 'ratio_ylabel_pad']:
-        val = getattr(plot_setup, param_name)
-        if val is not None:
-            roc_json_entry['plot_setup'][param_name] = val
+    # loop over [ROC curve, ROC curve WP] for a given discriminator and store its info into dict
+    for curve_type, curve in zip(['roc_curve', 'roc_wp'], [roc, wp_roc]):
+        if curve is None: continue
 
-    x_range = 1
-    for lim_name in [ 'x', 'y', 'ratio_y' ]:
-        lim = getattr(plot_setup, lim_name + 'lim')
-        if lim is not None:
-            lim = lim[pt_index] if type(lim[0]) == list else lim
-            roc_json_entry['plot_setup'][lim_name + '_min'] = lim[0]
-            roc_json_entry['plot_setup'][lim_name + '_max'] = lim[1]
-            if lim_name == 'x':
-                x_range = lim[1] - lim[0]
+        curve_data = {
+            'pt_min': pt_bins[pt_index], 'pt_max': pt_bins[pt_index + 1], 
+            'auc_score': curve.auc_score,
+            'false_positive_rate': eval_tools.FloatList(curve.pr[0, :].tolist()),
+            'true_positive_rate': eval_tools.FloatList(curve.pr[1, :].tolist()),
+        }
+        if curve.thresholds is not None:
+            curve_data['thresholds'] = eval_tools.FloatList(curve.thresholds.tolist())
+        if curve.pr_err is not None:
+            curve_data['false_positive_rate_up'] = eval_tools.FloatList(curve.pr_err[0, 0, :].tolist())
+            curve_data['false_positive_rate_down'] = eval_tools.FloatList(curve.pr_err[0, 1, :].tolist())
+            curve_data['true_positive_rate_up'] = eval_tools.FloatList(curve.pr_err[1, 0, :].tolist())
+            curve_data['true_positive_rate_down'] = eval_tools.FloatList(curve.pr_err[1, 1, :].tolist())
 
-    for n in reversed(range(n_discr)):
-        ref_roc = rocs[-1]
-        rocs[n], wp_rocs[n] = discriminators[n].CreateRocCurve(df_tx, ref_roc)
-        if rocs[n].auc_score is not None:
-            #target_prs = [0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 0.95, 0.99, 0.995 ]
-            #thrs = [ find_threshold(rocs[n].pr[1, :], rocs[n].thresholds, pr) for pr in target_prs ]
-            print('[{}, {}] {} roc_auc = {}'.format(pt_bins[pt_index], pt_bins[pt_index + 1], names[n],
-                                                    rocs[n].auc_score))
-            #print(thrs)
-        #print(discriminators[n].name)
-        name_suffix = ''
-        for roc in [ rocs[n].Prune(tpr_decimals=max(0, round(math.log10(1000 / x_range)))), wp_rocs[n] ]:
-            if roc is None: continue
+        # plot setup for the curve
+        curve_data['plot_setup'] = {
+            'color': curve.color,
+            'dots_only': curve.dots_only,
+            'dashed': curve.dashed,
+            'marker_size': curve.marker_size
+        }
+        curve_data['plot_setup']['ratio_title'] = 'MVA/DeepTau' if args.other_type != 'mu' else 'cut based/DeepTau'
 
-            discr_data = {
-                'name': discriminators[n].name + name_suffix,
-                'false_positive_rate': eval_tools.FloatList(roc.pr[0, :].tolist()),
-                'true_positive_rate': eval_tools.FloatList(roc.pr[1, :].tolist()),
-                'is_ref': n == n_discr - 1,
-                'color': roc.color,
-                'auc_score': roc.auc_score,
-                'dots_only': roc.dots_only,
-                'dashed': roc.dashed,
-                'marker_size': roc.marker_size,
-            }
-            if roc.thresholds is not None:
-                discr_data['thresholds'] = eval_tools.FloatList(roc.thresholds.tolist())
-            if roc.pr_err is not None:
-                discr_data['false_positive_rate_up'] = eval_tools.FloatList(roc.pr_err[0, 0, :].tolist())
-                discr_data['false_positive_rate_down'] = eval_tools.FloatList(roc.pr_err[0, 1, :].tolist())
-                discr_data['true_positive_rate_up'] = eval_tools.FloatList(roc.pr_err[1, 0, :].tolist())
-                discr_data['true_positive_rate_down'] = eval_tools.FloatList(roc.pr_err[1, 1, :].tolist())
-            roc_json_entry['discriminators'].insert(0, discr_data)
-            name_suffix = ' WP'
+        for lim_name in [ 'x', 'y', 'ratio_y' ]:
+            lim = getattr(plot_setup, lim_name + 'lim')
+            if lim is not None:
+                lim = lim[pt_index] if type(lim[0]) == list else lim
+                curve_data['plot_setup'][lim_name + '_min'] = lim[0]
+                curve_data['plot_setup'][lim_name + '_max'] = lim[1]
 
-    ratio_title = 'MVA/DeepTau' if args.other_type != 'mu' else 'cut based/DeepTau'
-    roc_json_entry['plot_setup']['ratio_title'] = ratio_title
-    roc_json_entry['period'] = '2017 (13 TeV)'
-    if args.public_plots:
-        if pt_bins[pt_index + 1] == 1000:
-            pt_text = r'$p_T > {}$ GeV'.format(pt_bins[pt_index])
-        elif pt_bins[pt_index] == 20:
-            pt_text = r'$p_T < {}$ GeV'.format(pt_bins[pt_index + 1])
-        else:
-            pt_text = r'$p_T\in ({}, {})$ GeV'.format(pt_bins[pt_index], pt_bins[pt_index + 1])
-        roc_json_entry['pt_text'] = pt_text
-    else:
-        if args.inequality_in_title and (pt_bins[pt_index] == 20 or pt_bins[pt_index + 1] == 1000) \
-                and not (pt_bins[pt_index] == 20 and pt_bins[pt_index + 1] == 1000):
-            if pt_bins[pt_index] == 20:
-                title_str = 'tau vs {}. pt < {} GeV'.format(args.other_type, pt_bins[pt_index + 1])
+        for param_name in [ 'ylabel', 'yscale', 'ratio_yscale', 'legend_loc', 'ratio_ylabel_pad']:
+            val = getattr(plot_setup, param_name)
+            if val is not None:
+                curve_data['plot_setup'][param_name] = val
+
+        if args.public_plots:
+            if pt_bins[pt_index + 1] == 1000:
+                pt_text = r'$p_T > {}$ GeV'.format(pt_bins[pt_index])
+            elif pt_bins[pt_index] == 20:
+                pt_text = r'$p_T < {}$ GeV'.format(pt_bins[pt_index + 1])
             else:
-                title_str = 'tau vs {}. pt > {} GeV'.format(args.other_type, pt_bins[pt_index])
+                pt_text = r'$p_T\in ({}, {})$ GeV'.format(pt_bins[pt_index], pt_bins[pt_index + 1])
+            curve_data['plot_setup']['pt_text'] = pt_text
         else:
-            title_str = 'tau vs {}. pt range ({}, {}) GeV'.format(args.other_type, pt_bins[pt_index],
-                                                                    pt_bins[pt_index + 1])
-        roc_json_entry['pt_text'] = title_str
-    roc_json.append(roc_json_entry)
+            if args.inequality_in_title and (pt_bins[pt_index] == 20 or pt_bins[pt_index + 1] == 1000) \
+                    and not (pt_bins[pt_index] == 20 and pt_bins[pt_index + 1] == 1000):
+                if pt_bins[pt_index] == 20:
+                    title_str = 'tau vs {}. pt < {} GeV'.format(args.other_type, pt_bins[pt_index + 1])
+                else:
+                    title_str = 'tau vs {}. pt > {} GeV'.format(args.other_type, pt_bins[pt_index])
+            else:
+                title_str = 'tau vs {}. pt range ({}, {}) GeV'.format(args.other_type, pt_bins[pt_index],
+                                                                        pt_bins[pt_index + 1])
+            curve_data['plot_setup']['pt_text'] = title_str
+
+        discr_json['metrics'][curve_type].append(curve_data)
 
 with open(args.output, 'w') as json_file:
-    json_file.write(json.dumps(roc_json, indent=4, cls=eval_tools.CustomJsonEncoder))
+    json_file.write(json.dumps(discr_json, indent=4, cls=eval_tools.CustomJsonEncoder))
