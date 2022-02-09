38b8e313b962690a58cf5363ebc1398a62377ff1
Luca Guzzi <luca.guzzi@cern.ch>
2022-02-09 10:13:03 +0100

    allow nan in feature scaling computation

--- Training/python/scaling_utils.py
+++ Training/python/scaling_utils.py
@@ -307,8 +307,8 @@ def fill_aggregators(tree, var, var_type, file_i, file_name_i, cone_type, cone_d
     """
     constituent_eta_name, constituent_phi_name = cone_selection_dict[var_type]['var_names']['eta'], cone_selection_dict[var_type]['var_names']['phi']
     var_array, constituent_eta_array, constituent_phi_array = tree.arrays([var, constituent_eta_name, constituent_phi_name], cut=selection_cut, aliases=aliases, how=tuple)
-    var_array = mask_inf(var_array, var, inf_counter, raise_exception=True)
-    var_array = mask_nan(var_array, var, nan_counter, raise_exception=True)
+    #var_array = mask_inf(var_array, var, inf_counter, raise_exception=True)
+    #var_array = mask_nan(var_array, var, nan_counter, raise_exception=True)
 
     if cone_type == None:
         sums[var_type][var][file_i] += ak.sum(var_array)
@@ -388,4 +388,4 @@ def dump_to_json(dict_map):
     """
     for fout_name, dict in dict_map.items():
         with open(f'{fout_name}.json', 'w') as fout:
-            json.dump(dict, fout, indent=4, allow_nan=False, sort_keys=False)
+            json.dump(dict, fout, indent=4, allow_nan=True, sort_keys=False)
