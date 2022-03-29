0cf225f5c7a33615001b6b263a72929a3ea824ae
Andrew Loeliger <loeliger@wisc.edu>
2022-03-29 14:31:33 -0500

    Post validation commit

--- Training/interface/DataLoader_main.h
+++ Training/interface/DataLoader_main.h
@@ -203,6 +203,7 @@ public:
         );
         if (debug) hist_weights[boostedTau_type]->SaveAs(("Temp_"+boostedTau_name+".root").c_str()); // It's required that all bins are filled in these histograms; save them to check incase binning is too fine and some bins are empty
 
+
         target_histogram.reset();
         input_histogram .reset();
       }
