42355291084c5813d1095404dcee64af2686aa70
Oleg Filatov <20295221+ofivite@users.noreply.github.com>
2022-05-24 11:02:33 +0200

    log wps to mlflow

--- Evaluation/derive_wp.py
+++ Evaluation/derive_wp.py
@@ -5,6 +5,7 @@ from dataclasses import dataclass
 import numpy as np
 import pandas as pd
 
+import mlflow
 import hydra
 from hydra.utils import to_absolute_path, instantiate, call
 from omegaconf import DictConfig, ListConfig
@@ -39,6 +40,9 @@ class WPMaker:
             for wp_cfg in WPs.values(): # for each WP per vs_type
                 wp_cfg['thrs'] = []
         self.__converged = False
+
+    def is_converged(self):
+        return self.__converged
 
     def apply_wp_vs_others(self, current_vs_type):
         df = self._taus
@@ -159,6 +163,28 @@ def main(cfg: DictConfig) -> None:
     wp_maker._taus = call(cfg.create_df)
     wp_maker.run()
     wp_maker.print_wp()
+
+    if wp_maker.is_converged():
+        if os.path.exists(path_to_mlflow:=to_absolute_path(cfg.create_df.path_to_mlflow)):
+            mlflow.set_tracking_uri(f"file://{path_to_mlflow}")
+            if mlflow.get_experiment(experiment_id:=str(cfg.create_df.experiment_id)) and mlflow.get_run(run_id:=cfg.create_df.run_id):
+                path_to_artifacts = f'{path_to_mlflow}/{experiment_id}/{run_id}/artifacts'
+                
+                # store threshold values for all iterations
+                with open(f'{path_to_artifacts}/working_points.log', 'w') as json_file:
+                    json_file.write(json.dumps(wp_maker.wp_definitions, indent=4))
+                
+                # store final thresholds
+                wp_definitions_final = {}
+                for vs_type, WPs in wp_maker.wp_definitions.items():
+                    wp_definitions_final[vs_type] = {wp_name: wp_cfg['thrs'][-1] for wp_name, wp_cfg in reversed(WPs.items())}
+
+                with open(f'{path_to_artifacts}/working_points.json', 'w') as json_file:
+                    json_file.write(json.dumps(wp_definitions_final, indent=4))
+
+                print(f'\n[INFO] logged working points to artifacts for experiment ({experiment_id}), run ({run_id})\n')
+    else:
+        print('\nWPMaker haven\'t converged!\n')
 
 if __name__ == '__main__':
     main()
