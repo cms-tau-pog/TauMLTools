3f4665da8fdbf2d7b53a6187ccd88c3d8bea92c2
Oleg Filatov <20295221+ofivite@users.noreply.github.com>
2022-06-16 11:45:34 +0200

    add reading from pre-skimmed dataframes

--- Evaluation/plot_wp_eff.py
+++ Evaluation/plot_wp_eff.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 import mlflow
 import hydra
 from hydra.utils import to_absolute_path, instantiate
@@ -12,6 +13,7 @@ def main(cfg: DictConfig) -> None:
     
     # read WP thresholds from mlflow artifacts
     path_to_mlflow = to_absolute_path(cfg['create_df']['path_to_mlflow'])
+    mlflow.set_tracking_uri(f"file://{path_to_mlflow}")
     experiment_id = cfg['create_df']['experiment_id']
     run_id = cfg['create_df']['run_id']
     path_to_wp_definitions = f"{path_to_mlflow}/{experiment_id}/{run_id}/artifacts/working_points.json"
@@ -25,10 +27,19 @@ def main(cfg: DictConfig) -> None:
     
     # make dataframes with specified input features and predictions for each tau_type
     vs_type = cfg['vs_type']
+    data_subfolder = 'wp_in_denom' if cfg['require_WPs_in_denominator'] else 'no_wp_in_denom'
     df_sel = {}
-    for tau_type in ['tau', vs_type]:
-        df = partial_create_df(tau_type_to_select=tau_type, pred_samples=cfg['pred_samples'][tau_type])
-        df_sel[tau_type] = df
+    if not cfg['from_skims']: # create dataframes and log to mlflow 
+        with mlflow.start_run(experiment_id=experiment_id, run_id=run_id) as active_run:
+            for tau_type in ['tau', vs_type]:
+                df = partial_create_df(tau_type_to_select=tau_type, pred_samples=cfg['pred_samples'][tau_type])
+                df.to_csv(f'{tau_type}.csv')
+                mlflow.log_artifact(f'{tau_type}.csv', f'wp_eff/data/{data_subfolder}')
+                df_sel[tau_type] = df
+    else: # read already existing skimmed dataframes
+        for tau_type in ['tau', vs_type]:
+            df = pd.read_csv(f"{path_to_mlflow}/{experiment_id}/{run_id}/artifacts/wp_eff/data/{data_subfolder}/{tau_type}.csv")
+            df_sel[tau_type] = df
 
     # compute and plot efficiency curves
     wp_thrs, wp_names = list(wp_definitions[vs_type].values()), list(wp_definitions[vs_type].keys())
@@ -42,7 +53,6 @@ def main(cfg: DictConfig) -> None:
     fig = partial_plot_efficiency(eff=eff, eff_up=eff_up, eff_down=eff_down, labels=wp_names)
 
     # log to mlflow
-    mlflow.set_tracking_uri(f"file://{path_to_mlflow}")
     with mlflow.start_run(experiment_id=experiment_id, run_id=run_id) as active_run:
         mlflow.log_figure(fig, cfg['output_filename'])
     print(f'\n[INFO] logged plots to artifacts for experiment ({experiment_id}), run ({run_id})\n')
