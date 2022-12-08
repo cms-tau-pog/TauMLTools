9fb03b475bd191e1cd7534bcddf47e2dfc9ca6e5
zhiyuanl <zhiyuanl@cern.ch>
2022-12-08 11:12:46 +0100

    adding variables in tau

--- Production/python/customiseHLT.py
+++ Production/python/customiseHLT.py
@@ -1,5 +1,7 @@
 # How to run:
+# hltGetConfiguration /dev/CMSSW_12_4_0/GRun --globaltag auto:phase1_2022_realistic --mc --unprescale --no-output --max-events 100 --eras Run3 --l1-emulator FullMC --l1 L1Menu_Collisions2022_v1_3_0-d1_xml --customise TauMLTools/Production/customiseHLT.customise --input file:00c642d1-bf7e-477d-91e1-4dd9ce2c8099.root > hltRun3Summer21MC.py
 # hltGetConfiguration /dev/CMSSW_12_4_0/GRun --globaltag auto:phase1_2022_realistic --mc --unprescale --no-output --max-events 100 --eras Run3 --l1-emulator FullMC --l1 L1Menu_Collisions2022_v1_3_0-d1_xml --customise TauMLTools/Production/customiseHLT.customise --input /store/mc/Run3Summer21DRPremix/TT_TuneCP5_14TeV-powheg-pythia8/GEN-SIM-DIGI-RAW/120X_mcRun3_2021_realistic_v6-v2/2540000/b354245e-d8bc-424d-b527-58815586a6a5.root > hltRun3Summer21MC.py
+
 # cmsRun hltRun3Summer21MC.py
 # file dataset=/store/mc/Run3Summer21DRPremix/TT_TuneCP5_14TeV-powheg-pythia8/GEN-SIM-DIGI-RAW/120X_mcRun3_2021_realistic_v6-v2/2540000/00c642d1-bf7e-477d-91e1-4dd9ce2c8099.root
 import FWCore.ParameterSet.Config as cms
@@ -71,48 +73,80 @@ def customise(process):
       vx = Var("vx", float, doc='x coordinate of vertex position'),
       vy = Var("vy", float, doc='y coordinate of vertex position'),
       vz = Var("vz", float, doc='z coordinate of vertex position'),
+
+
+      # all Tau_muonDecision is 1? 
+
       # these variables are 0
-      # vertexChi2 = Var("vertexChi2", float, doc='chi-squares'),
-      # vertexNdof = Var("vertexNdof", float, doc='Number of degrees of freedom,  Meant to be Double32_t for soft-assignment fitters'),
-      # status = Var("status", int, doc='status word'),
-      # longLived = Var("longLived", bool, doc='is long lived?'),
-      # massConstraint = Var("massConstraint", bool, doc='do mass constraint?'),
+      # Tau_chargedEmEnergy
+      # Tau_electronEnergy
+      # Tau_electronMultiplicity
+      # Tau_electronPreIDDecision
+      # Tau_HFHadronEnergy
+      # Tau_HFHadroMultiplicity
+      # Tau_hoEnergy
+      # Tau_secondaryVertex_t
+      # Tau_vertexChi2 = Var("vertexChi2", float, doc='chi-squares'),
+      # Tau_vertexNdof = Var("vertexNdof", float, doc='Number of degrees of freedom,  Meant to be Double32_t for soft-assignment fitters'),
+      # nPFCand
+      # PFCand_vertexChi2
+      # PFCand_vertexNdof
+      # PFCand_longLived = Var("longLived", bool, doc='is long lived?'),
+      # PFCand_trackMissingInnerHits = Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.missingInnerHits : -999.", float, doc = "number of hits expected from inner track extrapolation but missing"),
+      # PFCand_trackMissingOuterHits= Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.missingOuterHits : -999.", float, doc = " number of hits expected from outer track extrapolation but missing"),
+      # PFCand_status = Var("status", int, doc='status word'),
+      # PFCand_longLived = Var("longLived", bool, doc='is long lived?'),
+      # PFCand_massConstraint = Var("massConstraint", bool, doc='do mass constraint?'),
+
       pdgId = Var("pdgId", int, doc='PDG identifier'),
+      dz = Var("? leadPFCand.trackRef.isNonnull && leadPFCand.trackRef.isAvailable ? leadPFCand.trackRef.dz : -999 ", float, doc='lead PF Candidate dz'),
+      dzError = Var("? leadPFCand.trackRef.isNonnull && leadPFCand.trackRef.isAvailable ? leadPFCand.trackRef.dzError : -999 ", float, doc='lead PF Candidate dz Error'),
       decayMode = Var("decayMode", int, doc='tau decay mode'),
       tauIsValid = Var("jetRef.isNonnull && jetRef.isAvailable", bool, doc = "tau is valid"),
       # variables available in PF jets
-      tauChargedHadronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedHadronEnergy : -999.", float, doc = "chargedHadronEnergy"),
-      tauNeutralHadronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralHadronEnergy : -999.", float, doc = "neutralHadronEnergy"),
-      tauPhotonEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.photonEnergy : -999.", float, doc = "photonEnergy"),
-      tauElectronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.electronEnergy : -999.", float, doc = "electronEnergy"),
-      tauMuonEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.muonEnergy : -999.", float, doc = "muonEnergy"),
-      tauHFHadronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFHadronEnergy : -999.", float, doc = "HFHadronEnergy"),
-      tauHFEMEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFEMEnergy : -999.", float, doc = "HFEMEnergy"),
-      tauChargedHadronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedHadronMultiplicity : -999.", float, doc = "chargedHadronMultiplicity"),
-      tauNeutralHadronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralHadronMultiplicity : -999.", float, doc = "neutralHadronMultiplicity"),
-      tauPhotonMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.photonMultiplicity : -999.", float, doc = "photonMultiplicity"),
-      tauElectronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.electronMultiplicity : -999.", float, doc = "electronMultiplicity"),
-      tauMuonMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.muonMultiplicity : -999.", float, doc = "muonMultiplicity"),
-      tauHFHadronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFHadronMultiplicity : -999.", float, doc = "HFHadronMultiplicity"),
-      tauHFEMMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFEMMultiplicity : -999.", float, doc = "HFEMMultiplicity"),
-      tauChargedEmEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedEmEnergy : -999.", float, doc = "chargedEmEnergy"),
-      tauChargedMuEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedMuEnergy : -999.", float, doc = "chargedMuEnergy"),
-      tauNeutralEmEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralEmEnergy : -999.", float, doc = "neutralEmEnergy"),
-      tauhoEnergy =  Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.hoEnergy : -999.", float, doc = "hoEnergy"),
-      tauChargedMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedMultiplicity : -999.", float, doc = "chargedMultiplicity"),
-      tauNeutralMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralMultiplicity : -999.", float, doc = "neutralMultiplicity"),
-      
-      ## variables available in PF tau
-      tauEmFraction = Var("emFraction", float, doc = " Ecal/Hcal Cluster Energy"),
-      tauHcalTotOverPLead = Var("hcalTotOverPLead", float, doc = " total Hcal Cluster E / leadPFChargedHadron P"),
-      tauSignalConeSize = Var("signalConeSize", float, doc = "Size of signal cone"),
+      # DataFormats/JetReco/interface/PFJet.h
+      chargedHadronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedHadronEnergy : -999.", float, doc = "chargedHadronEnergy"),
+      neutralHadronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralHadronEnergy : -999.", float, doc = "neutralHadronEnergy"),
+      photonEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.photonEnergy : -999.", float, doc = "photonEnergy"),
+      electronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.electronEnergy : -999.", float, doc = "electronEnergy"),
+      muonEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.muonEnergy : -999.", float, doc = "muonEnergy"),
+      HFHadronEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFHadronEnergy : -999.", float, doc = "HFHadronEnergy"),
+      HFEMEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFEMEnergy : -999.", float, doc = "HFEMEnergy"),
+      chargedHadronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedHadronMultiplicity : -999.", float, doc = "chargedHadronMultiplicity"),
+      neutralHadronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralHadronMultiplicity : -999.", float, doc = "neutralHadronMultiplicity"),
+      photonMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.photonMultiplicity : -999.", float, doc = "photonMultiplicity"),
+      electronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.electronMultiplicity : -999.", float, doc = "electronMultiplicity"),
+      muonMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.muonMultiplicity : -999.", float, doc = "muonMultiplicity"),
+      HFHadronMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFHadronMultiplicity : -999.", float, doc = "HFHadronMultiplicity"),
+      HFEMMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.HFEMMultiplicity : -999.", float, doc = "HFEMMultiplicity"),
+      chargedEmEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedEmEnergy : -999.", float, doc = "chargedEmEnergy"),
+      chargedMuEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedMuEnergy : -999.", float, doc = "chargedMuEnergy"),
+      neutralEmEnergy = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralEmEnergy : -999.", float, doc = "neutralEmEnergy"),
+      chargedMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.chargedMultiplicity : -999.", float, doc = "chargedMultiplicity"),
+      neutralMultiplicity = Var("? jetRef.isNonnull && jetRef.isAvailable ? jetRef.neutralMultiplicity : -999.", float, doc = "neutralMultiplicity"),
+      # # source: DataFormats/TauReco/interface/PFTau.h
+      # ## variables available in PF tau
+      emFraction = Var("emFraction", float, doc = " Ecal/Hcal Cluster Energy"),
+      hcalTotOverPLead = Var("hcalTotOverPLead", float, doc = " total Hcal Cluster E / leadPFChargedHadron P"),
+      signalConeSize = Var("signalConeSize", float, doc = "Size of signal cone"),
       # lepton decision variables
-      tauElectronPreIDDecision = Var("electronPreIDDecision", bool, doc = " Decision from Electron PreID"),
-      tauMuonDecision = Var("muonDecision", bool, doc = "muonDecision"),
+      electronPreIDDecision = Var("electronPreIDDecision", bool, doc = " Decision from Electron PreID"),
+      muonDecision = Var("muonDecision", bool, doc = "muonDecision"),
+
+
 
     )
   )
   # cms.EDProducer : an object that produces a new data object
+  process.tauExtTable = cms.EDProducer("TauTableProducerHLT",
+    taus = cms.InputTag( "hltHpsPFTauProducer" ),
+    deepTauVSe = cms.InputTag("hltHpsPFTauDeepTauProducerForVBFIsoTau", "VSe"),
+    deepTauVSmu = cms.InputTag("hltHpsPFTauDeepTauProducerForVBFIsoTau", "VSmu"),
+    deepTauVSjet = cms.InputTag("hltHpsPFTauDeepTauProducerForVBFIsoTau", "VSjet"),
+    tauTransverseImpactParameters = cms.InputTag( "hltHpsPFTauTransverseImpactParametersForDeepTauForVBFIsoTau" ),
+    precision = cms.int32(7),
+  )
+
 
   process.pfCandTable = cms.EDProducer("SimpleCandidateFlatTableProducer",
     src = cms.InputTag( "hltParticleFlowForTaus" ),
@@ -154,9 +188,6 @@ def customise(process):
       # source: DataFormats/TrackReco/interface/TrackBase.h
       trackNumberOfValidHits = Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.numberOfValidHits : -999.", float, doc = "number of valid hits found"),
       trackNumberOfLostHits = Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.numberOfLostHits : -999.", float, doc = " number of cases where track crossed a layer without getting a hit."),
-      # this two variables gives 0 for all events:
-      # trackMissingInnerHits = Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.missingInnerHits : -999.", float, doc = "number of hits expected from inner track extrapolation but missing"),
-      # trackMissingOuterHits= Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.missingOuterHits : -999.", float, doc = " number of hits expected from outer track extrapolation but missing"),
       trackHitsValidFraction= Var("? trackRef.isNonnull && trackRef.isAvailable ? trackRef.validFraction : -999.", float, doc = "fraction of valid hits on the track"),
 
       rawHcalEnergy = Var("rawHcalEnergy", float, doc='rawHcalEnergy'),
@@ -167,7 +198,8 @@ def customise(process):
     )
   )
 
-  process.tauTablesTask = cms.Task(process.tauTable)
+  
+  process.tauTablesTask = cms.Task(process.tauTable, process.tauExtTable)
   process.pfCandTablesTask = cms.Task(process.pfCandTable)
   process.nanoTableTaskFS = cms.Task(process.genParticleTablesTask, process.genParticleTask,
                                      process.tauTablesTask, process.pfCandTablesTask)
