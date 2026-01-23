97f904f99bb9c38f3afc11116ee7bf22505dbc4c
Tim Voigtländer <tvoigtlaender@bms1.etp.kit.edu>
2026-01-23 01:01:47 +0100

    Adjust wp/performance evaluation scripts for personal file structure. This breaks other types of file processing.

--- Evaluation/utils/data_processing.py
+++ Evaluation/utils/data_processing.py
@@ -1,6 +1,6 @@
 import os
-import json 
-from glob import glob 
+import json
+from glob import glob
 from omegaconf import ListConfig
 from hydra.utils import to_absolute_path
 
@@ -11,25 +11,28 @@ import uproot
 def tau_vs_other(prob_tau, prob_other):
     return np.where(prob_tau > 0, prob_tau / (prob_tau + prob_other), np.zeros(prob_tau.shape))
 
-def create_df(path_to_preds, pred_samples, tau_type_to_select, selection, 
-              pred_group_name, pred_column_prefix, target_group_name, target_column_prefix, 
-              add_columns_from, add_columns, group_or_tree_name, 
+def create_df(path_to_preds, pred_samples, tau_type_to_select, selection,
+              pred_group_name, pred_column_prefix, target_group_name, target_column_prefix,
+              add_columns_from, add_columns, group_or_tree_name,
               **kwargs):
     df = []
-    path_to_preds = os.path.abspath(to_absolute_path(path_to_preds))
+    pred_paths = glob(path_to_preds)
+    if len(pred_paths) != 1:
+        raise Exception(f"Found {len(pred_paths)} predictions in {path_to_preds}")
+    path_to_preds = to_absolute_path(pred_paths[0])
 
     # loop over input samples
     for sample_name, sample_cfg in pred_samples.items():
 
         # make list of files with predictions depending on the specified format
         filename_pattern = sample_cfg['filename_pattern']
-        if isinstance(filename_pattern, ListConfig): 
+        if isinstance(filename_pattern, ListConfig):
             pred_files = [f'{path_to_preds}/{sample_name}/{filename}' for filename in filename_pattern]
         elif isinstance(filename_pattern, str):
             pred_files = glob(f'{path_to_preds}/{sample_name}/{filename_pattern}')
         else:
             raise Exception(f"unknown type of filename_pattern: {type(filename_pattern)}")
-        
+
         df_sample = []
         for pred_file in pred_files:
             # read predictions and labels
@@ -44,11 +47,11 @@ def create_df(path_to_preds, pred_samples, tau_type_to_select, selection,
             if add_columns_from == 'inputs':# read add_columns from the corresponding input file
                 json_filemap_name = f'{path_to_preds}/{sample_name}/pred_input_filemap.json'
                 with open(json_filemap_name, 'r') as json_file:
-                    target_input_map = json.load(json_file) 
+                    target_input_map = json.load(json_file)
 
                 with uproot.open(target_input_map[pred_file]) as f:
                     df_add = f[group_or_tree_name].arrays(add_columns, library='pd')
-                
+
                 # concatenate input branches and predictions/labels
                 assert df_pred.shape[0] == df_add.shape[0], "Sizes of prediction and input dataframes don't match."
             elif add_columns_from == 'predictions':
@@ -60,7 +63,7 @@ def create_df(path_to_preds, pred_samples, tau_type_to_select, selection,
             df_sample_ = pd.concat([df_pred, df_add], axis=1)
             assert not any(df_sample_.isna().any(axis=0)), 'found NaNs!'
             df_sample.append(df_sample_)
-        
+
         # concat together files for a given sample and apply tau_type matching & selection
         df_sample = pd.concat(df_sample, axis=0)
         df_sample = df_sample.query(f'{target_group_name}_{target_column_prefix}{tau_type_to_select}==1')
@@ -69,9 +72,9 @@ def create_df(path_to_preds, pred_samples, tau_type_to_select, selection,
 
         # compute weight variable
         df_sample['weight'] = 1.
-        if sample_cfg['reweight_to_lumi'] is not None:
+        if sample_cfg.get('reweight_to_lumi'):
             df_sample['weight'] *= sample_cfg['sample_lumi'] / (sample_cfg['reweight_to_lumi'] * df_sample.shape[0])
-        
+
         df.append(df_sample)
 
     # concat across samples
@@ -81,6 +84,6 @@ def create_df(path_to_preds, pred_samples, tau_type_to_select, selection,
     vs_types = ['e', 'mu', 'jet']
     for vs_type in vs_types:
         df['score_vs_' + vs_type] = tau_vs_other(df[f'{pred_group_name}_{pred_column_prefix}tau'].values, df[f'{pred_group_name}_{pred_column_prefix}' + vs_type].values)
-   
+
     print(f'\n-> Selected {df.shape[0]} {tau_type_to_select}s')
     return df
